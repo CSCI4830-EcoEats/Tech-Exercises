c097a60345e44004b403e8f57e3472832d444bd6
sophiafharding <142829755+sophiafharding@users.noreply.github.com>
2025-06-27 21:47:06 -0500

    Added name and comment to Hello EcoEats line

--- Tech Exercise 4/Hello_World/Hello_World_App/views.py
+++ Tech Exercise 4/Hello_World/Hello_World_App/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello_world(request):
-    return HttpResponse("Hello EcoEats!")
+    return HttpResponse("Hello EcoEats! From Sophia :)") #Changed this line here
 
 # Create your views here.
 def hello_EcoEats(request):
