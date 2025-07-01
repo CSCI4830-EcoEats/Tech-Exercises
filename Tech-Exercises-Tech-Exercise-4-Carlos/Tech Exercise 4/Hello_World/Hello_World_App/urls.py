298b1eb7e485f00148418e32cfed0a4cda71bca7
Carlos3443 <151295984+Carlos3443@users.noreply.github.com>
2025-07-01 16:12:58 -0500

    Delete Tech-Exercises-Tech-Exercise-4-Carlos directory

--- Tech-Exercises-Tech-Exercise-4-Carlos/Tech Exercise 4/Hello_World/Hello_World_App/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import hello_EcoEats, hello_world
-
-urlpatterns = [
-    path('', hello_world, name='hello_EcoEat'),
-    path('hello', hello_world, name='hello_world'),
-]
